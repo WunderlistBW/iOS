c919f15de12435c9e0605523245da64d5a347e4e
Cody Morley <codymorleyinc@gmail.com>
2020-06-21 02:20:30 -0700

    UI - Onboarding - Login/Signup VC
    finished adding IBOutlets

--- Wunderlist/Wunderlist/View Controllers/Onboarding/LoginSignUpViewController.swift
+++ Wunderlist/Wunderlist/View Controllers/Onboarding/LoginSignUpViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class LoginSignUpViewController: UIViewController {
     //MARK: - Properties -
-    
+    ///later if we want to add a logo we can just substitute the placeholder photo using this property.
+    @IBOutlet weak var logoView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var rememberMeButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var anchorView: UIView!
     
     var loggingIn: Bool = true
     
