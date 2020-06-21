39e80235b03418bef7cc8ca1901ee9d7fa00ffba
Cody Morley <codymorleyinc@gmail.com>
2020-06-20 17:14:25 -0700

    UI - Onboarding
    Devised a skeleton for onboarding. Contains 2 VCs and a storyboard file.

---
+++ Wunderlist/Wunderlist/View Controllers/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingViewController.swift
+//  Wunderlist
+//
+//  Created by Bling Morley on 6/20/20.
+//  Copyright © 2020 Wunderlist. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
