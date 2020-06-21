3042e2eb301a8aa152286a5087169841294a47eb
Cody Morley <codymorleyinc@gmail.com>
2020-06-21 01:06:12 -0700

    UI - Onboarding
    finished almost all of the coding for the onboarding view controller. will need to add photos for the welcome slide show. Moving on to storyboard for login/signup end of the flow.

--- Wunderlist/Wunderlist/View Controllers/Onboarding/OnboardingViewController.swift
+++ Wunderlist/Wunderlist/View Controllers/Onboarding/OnboardingViewController.swift
@@ -6,6 +6,25 @@
 //  Copyright © 2020 Wunderlist. All rights reserved.
 //
 
+/*
+ TODO LIST
+ //unfinished
+ Get photos for welcome slideshow and add them to asset folder
+ Give the photos a naming convention that makes them easy to add to the page control view
+ 
+ 
+ //finished
+ properties list
+ set scroll view delegate
+ set up so photos in slideshow will snap into place with frames
+ set button to use app without logging in (limit functionality? I don't know. Question for later.)
+ set user defaults to easily capture login flow for future logins
+ set segues to fork onboarding for login/signup
+ Set up update views logic to use imageFrame to operate slideshow inside of scrollView view
+ */
+
+
+
 import UIKit
 
 class OnboardingViewController: UIViewController, UIScrollViewDelegate {
@@ -21,6 +40,7 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     //MARK: - Life Cycles -
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
     }
     
 
@@ -47,7 +67,22 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     
     //MARK: - Methods -
     private func updateViews() {
+        navigationController?.isNavigationBarHidden = true
+        pageControl.numberOfPages = welcomeSlideShow.count
         
+        for slide in 0..<welcomeSlideShow.count {
+            imageFrame.size = scrollView.frame.size
+            imageFrame.origin.x = scrollView.frame.size.width * CGFloat(slide)
+            
+            let slideImage = UIImageView(frame: imageFrame)
+            ///TODO: need to set image on the next line
+            //slideImage.image = UIImage(named: "")
+            slideImage.layer.masksToBounds = true
+            slideImage.contentMode = .scaleAspectFill
+            self.scrollView.addSubview(slideImage)
+        }
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(welcomeSlideShow.count),
+                                        height: scrollView.frame.size.height)
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
