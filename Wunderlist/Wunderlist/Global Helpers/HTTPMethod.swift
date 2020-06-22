ed540d2dd52c706a10d6328bfca089022cb99516
Chris Dobek <dobek.christopher@gmail.com>
2020-06-22 15:00:28 -0400

    HTTPMethod swift file

---
+++ Wunderlist/Wunderlist/Global Helpers/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  Wunderlist
+//
+//  Created by Chris Dobek on 6/22/20.
+//  Copyright © 2020 Wunderlist. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
