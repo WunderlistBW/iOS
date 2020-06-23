104693584ef0ae7176dd87cb03885fe3d8cf48f9
Cody Morley <codymorleyinc@gmail.com>
2020-06-22 17:44:54 -0700

    Tests
    Added @testable import

--- Wunderlist/WunderlistTests/WunderlistTests.swift
+++ Wunderlist/WunderlistTests/WunderlistTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Wunderlist
 
 class WunderlistTests: XCTestCase {
 
--- Wunderlist/WunderlistUITests/WunderlistUITests.swift
+++ Wunderlist/WunderlistUITests/WunderlistUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Wunderlist
 
 class WunderlistUITests: XCTestCase {
 
