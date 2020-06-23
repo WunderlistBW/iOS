1ad7ac93616145dc72013845a7b5c084534ef8af
nonye <nnezekwo@gmail.com>
2020-06-23 14:37:43 -0700

    Add Scenes to Storyboard, added AddViewController file & worked on DetailViewController

--- Wunderlist/Wunderlist/View Controllers/List Views/DetailViewController.swift
+++ Wunderlist/Wunderlist/View Controllers/List Views/DetailViewController.swift
@@ -13,13 +13,65 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var entryTitleField: UITextField!
     @IBOutlet weak var entryTextView: UITextView!
     @IBOutlet weak var entryDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
+        navigationItem.rightBarButtonItem = editButtonItem
     }
+//    private init() {
+//    }
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    // MARK: - PROPERTIES
+    var listController: ListController?
+    var listEntryController = ListEntryController()
+    var wasEdited = false
+    var listEntry: ListEntry?
     // MARK: - ACTIONS
     @IBAction func saveButtonTapped(_ sender: Any) {
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if wasEdited {
+            guard let entryTitle = entryTitleField.text,
+                !entryTitle.isEmpty,
+                let entryText = entryTextView.text,
+                !entryText.isEmpty,
+                let listEntry = listEntry else { return }
+            let name = entryTitleField.text
+            listEntry.name = name
+            // TODO:
+//            ListController.updateList(<#ListController#>)
+//
+//            do {
+//                try CoreDataStack.shared.save(in: listEntry)
+//            } catch {
+//                NSLog("Error saving managed object context (during list edit): \(error)")
+//            }
+        }
+    }
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        if editing { wasEdited = true }
+        entryTitleField.isUserInteractionEnabled = editing
+        entryTextView.isUserInteractionEnabled = editing
+        entryDatePicker.isUserInteractionEnabled = editing
+        navigationItem.hidesBackButton = editing
+    }
+    
+    private func updateViews() {
+        guard let listEntry = listEntry else { return }
+        entryTitleField.text = listEntry.name
+        entryTitleField.isUserInteractionEnabled = isEditing
+        entryTextView.isUserInteractionEnabled = isEditing
+        #warning("Bad unwrapping")
+        entryDatePicker.date = listEntry.dueDate!
+        entryDatePicker.isUserInteractionEnabled = isEditing
+        
     }
     /*
     // MARK: - Navigation
