8f3f43260a8ffafed9d158de0085a4d583a7fa61
nonye <nnezekwo@gmail.com>
2020-06-23 10:15:10 -0700

    UI Updates - Table View & Detail View created.

---
+++ Wunderlist/Wunderlist/View Controllers/List Views/ListCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListCell.swift
+//  Wunderlist
+//
+//  Created by Nonye on 6/23/20.
+//  Copyright © 2020 Wunderlist. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+    
+    var listEntryController = ListEntryController()
+    var isCompleted: Bool = false
+    
+    
+    // MARK: OUTLETS
+    @IBOutlet weak var reminderName: UILabel!
+    @IBOutlet weak var reminderButton: UIButton!
+    
+    var listEntry: ListEntry? {
+        didSet {
+            
+        }
+    }
+}
+
+
