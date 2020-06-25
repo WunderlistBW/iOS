85762cb46c113ee26863f5c37324572819101165
Cody Morley <codymorleyinc@gmail.com>
2020-06-25 11:50:31 -0700

    UI Tests - Logic
    finished pre-code logic flow for UITests. Next step is adding necessary string identifiers to storyboard elements.

--- Wunderlist/WunderlistUITests/WunderlistUITests.swift
+++ Wunderlist/WunderlistUITests/WunderlistUITests.swift
@@ -15,29 +15,75 @@ class WunderlistUITests: XCTestCase {
     
     // MARK: - Methods -
     override func setUp() {
-        <#code#>
+        let app = XCUIApplication()
+        app.launchArguments = ["UITesting"]
+        continueAfterFailure = false
+        app.launch()
     }
     
-    override func tearDown() {
-        <#code#>
-    }
     // MARK: - Tests -
-
     func testNewUserSignup() {
+        //launch app
+        //tap signup
+        //enter text in text boxes
+        // tap submit
+        //test you're on home tableview
     }
-
-    func testUserLogin() {
+    func testUserLoginAndRememberMe() {
+        //launch app
+        //tap login
+        //enter text in text boxes
+        //tap remember me
+        //tap submit
+        //test you're on home view
+        //close app
+        //launch app
+        //tap login
+        //test if testboxes have text autfilled
+        //tap submit
+        //test if back at home view
     }
 
     func testPasswordIncorrect() {
+        //launch app
+        //tap login
+        //enter text in text boxes, password incorrect
+        //test if still on login screen and status bar displays correct message
     }
 
     func testCreateListEntry() {
+        //launch app
+        //tap signup
+        //enter text in text boxes
+        //tap submit
+        
+        //tap add button
+        //tap cancel
+        //test at home tableview
+        //test no new list
+        
+        //tap add button
+        //enter text in text field
+        //select date from picker
+        //tap save
+        //tap finished
+        //test at home tableview
+        //test new list entry is available
+        //test UI element in tableview is in right mode for new entry
     }
 
     func testDeleteListEntry() {
-    }
-
-    func testCheckUserListEntries() {
+        //launch app
+        //tap signup
+        //enter text in text boxes
+        //tap submit
+        //tap add button
+        //enter text in text field
+        //select date from picker
+        //tap save
+        //tap finished
+        //test new list entry is available
+        //slide to delete
+        //test new list entry is no longer present
     }
 }
